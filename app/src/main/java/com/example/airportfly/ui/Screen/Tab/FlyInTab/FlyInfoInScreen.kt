99b52d3f014036ce98d067aa01e2517bde396251
Cheng-Yu Chung <chengyu.chung@gosaico.com>
2024-11-22 13:47:45 +0800

    Fixed CircularProgressIndicator not in the middle position

--- app/src/main/java/com/example/airportfly/ui/Screen/Tab/FlyInTab/FlyInfoInScreen.kt
+++ app/src/main/java/com/example/airportfly/ui/Screen/Tab/FlyInTab/FlyInfoInScreen.kt
@@ -1,38 +1,18 @@
 package com.example.airportfly.ui.Screen.Tab.FlyInTab
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.airportfly.Utility
-import com.example.airportfly.model.FlightInfo
-import com.example.airportfly.model.FlightStatus
 import com.example.airportfly.ui.Composable.FlightList
-import com.example.airportfly.ui.Composable.FlightListItem
-import com.example.airportfly.ui.Composable.StatusChip
 import com.example.airportfly.ui.Screen.FlightScheduleViewModel
 import com.example.airportfly.ui.Screen.Tab.FlightScheduleUiState
-import kotlinx.coroutines.delay
 
 
 @Composable
@@ -41,31 +21,26 @@ fun FlyInfoInScreen(
 ) {
     val flyInState by viewModel.flyInScheduleState.collectAsState()
 
-//    LaunchedEffect(isVisible) {
-//        if(isVisible) {
-//            viewModel.fetchInboundFlights()
-//
-//            while(true) {
-//                delay(Utility.REFRESH_INTERVAL)
-//                if(isVisible) {
-//                    viewModel.fetchInboundFlights()
-//                }
-//            }
-//        }
-//    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        when (val state = flyInState) {
+            is FlightScheduleUiState.Loading -> {
+                CircularProgressIndicator()
+            }
 
-    when (val state = flyInState) {
-        is FlightScheduleUiState.Loading -> {
-            CircularProgressIndicator()
-        }
-        is FlightScheduleUiState.Success -> {
-            FlightList(flights = state.flights)
-        }
-        is FlightScheduleUiState.Error -> {
-            Text(
-                text = "載入資料失敗: ${state.message}",
-                color = MaterialTheme.colorScheme.error
-            )
+            is FlightScheduleUiState.Success -> {
+                FlightList(flights = state.flights)
+            }
+
+            is FlightScheduleUiState.Error -> {
+                Text(
+                    text = "載入資料失敗: ${state.message}",
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/airportfly/ui/Screen/Tab/FlyOutTab/FlyInfoOutScreen.kt
+++ app/src/main/java/com/example/airportfly/ui/Screen/Tab/FlyOutTab/FlyInfoOutScreen.kt
@@ -1,34 +1,43 @@
 package com.example.airportfly.ui.Screen.Tab.FlyOutTab
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import com.example.airportfly.Utility
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.example.airportfly.ui.Composable.FlightList
 import com.example.airportfly.ui.Screen.FlightScheduleViewModel
 import com.example.airportfly.ui.Screen.Tab.FlightScheduleUiState
-import kotlinx.coroutines.delay
 
 @Composable
 fun FlyInfoOutScreen(viewModel: FlightScheduleViewModel) {
     val flyOutState by viewModel.flyOutScheduleState.collectAsState()
 
-    when (val state = flyOutState) {
-        is FlightScheduleUiState.Loading -> {
-            CircularProgressIndicator()
-        }
-        is FlightScheduleUiState.Success -> {
-            FlightList(flights = state.flights)
-        }
-        is FlightScheduleUiState.Error -> {
-            Text(
-                text = "載入資料失敗: ${state.message}",
-                color = MaterialTheme.colorScheme.error
-            )
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        when (val state = flyOutState) {
+            is FlightScheduleUiState.Loading -> {
+                CircularProgressIndicator()
+            }
+
+            is FlightScheduleUiState.Success -> {
+                FlightList(flights = state.flights)
+            }
+
+            is FlightScheduleUiState.Error -> {
+                Text(
+                    text = "載入資料失敗: ${state.message}",
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
         }
     }
 }
